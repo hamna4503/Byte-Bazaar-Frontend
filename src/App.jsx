d4c44f8ebefe4c8253d4d42987c74e605960aeb7
ummeabiha <rizvi4400977@cloud.neduet.edu.pk>
2024-03-10 04:32:18 +0500

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from "react";
-import ItemCard from "./components/itemCard/ItemCard";
-import xboxController from "./assets/images/items/controllers/xboxController.png";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import ItemCard from "./components/itemCard/ItemCard";
+// import xboxController from "./assets/images/items/controllers/xboxController.png";
+import Navbar from "./components/Navbar/Navbar";
 
-function App() {
+export default function App() {
   return (
-    // <h1>Check VSC HEHE</h1>
-    //WAS CHECKING MY ITEM CARD YOU CAN COMMENT IT OUT OR CHECK FOR YOURSELF. IT IS ONLY DEV FOR SMOL SCREENS.
-    <ItemCard
-      itemImg={xboxController}
-      itemName="XBOX 360 Controller"
-      itemCategory="Gaming Controller"
-      itemPrice="4500"
-      alt="XBOX-Controller"
-    />
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <ItemCard
+        itemImg={xboxController}
+        itemName="XBOX 360 Controller"
+        itemCategory="Gaming Controller"
+        itemPrice="4500"
+        alt="XBOX-Controller"
+      /> */}
+    </>
   );
 }
-
-export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,180 @@
-import React from "react";
-export default function Navbar(){
+/** @format */
+"use client";
 
-};
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Logo from "../../assets/images/navbar/logo.png";
+
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoCloseOutline, IoPersonCircle } from "react-icons/io5";
+import clsx from "clsx";
+
+export default function Navbar() {
+  const [isSideMenuOpen, setMenu] = useState(false);
+  const [isCategoriesOpen, setCategoriesOpen] = useState(false);
+
+  const navlinks = [
+    {
+      label: "Home",
+      link: "/",
+    },
+    {
+      label: "Categories",
+      link: "#",
+      dropdownOptions: [
+        "Computer Gadgets",
+        "Electronic Accessories",
+        "Gaming Items",
+      ],
+    },
+    {
+      label: "About",
+      link: "/",
+    },
+    {
+      label: "Contact",
+      link: "/",
+    },
+  ];
+
+  return (
+    <main>
+      <nav className="flex justify-between px-8 items-center py-6   ">
+        <div className="flex items-center gap-8">
+          <section className="flex items-center gap-4">
+            {/* menu */}
+            <FiMenu
+              onClick={() => setMenu(true)}
+              className="text-3xl cursor-pointer lg:hidden"
+            />
+            {/* logo */}
+            <Link to="/" className="text-8xl font-mono flex items-center">
+              <img
+                width={40}
+                height={40}
+                className="h-8 w-8 rounded-full mr-2 lg:block"
+                src={Logo}
+                alt="avatar-img"
+              />
+              <span className="text-xl font-bold lg:block hidden">
+                ByteBazaar
+              </span>
+            </Link>
+          </section>
+          {navlinks.map((d, i) => (
+            <div key={i} className="relative">
+              {d.label === "Categories" ? (
+                <Link
+                  to={d.link}
+                  className="hidden lg:block text-gray-400 hover:text-black"
+                  onClick={() => setCategoriesOpen(!isCategoriesOpen)}
+                >
+                  {d.label}
+                </Link>
+              ) : (
+                <Link
+                  to={d.link}
+                  className="hidden lg:block text-gray-400 hover:text-black"
+                >
+                  {d.label}
+                </Link>
+              )}
+              {d.dropdownOptions &&
+                d.label === "Categories" &&
+                isCategoriesOpen && (
+                  <div className="absolute top-full left-0 mt-2 bg-white rounded-md shadow-lg">
+                    {d.dropdownOptions.map((option, index) => (
+                      <Link
+                        key={index}
+                        to={`#${option.toLowerCase()}`}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        {option}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+            </div>
+          ))}
+        </div>
+
+        {/* Sidebar mobile menu */}
+        <div
+          className={clsx(
+            "fixed h-full w-screen lg:hidden bg-black/50  backdrop-blur-sm top-0 right-0  -translate-x-full  transition-all ",
+            isSideMenuOpen && "translate-x-0"
+          )}
+        >
+          <section className="text-black bg-white flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 w-56 flex  ">
+            <IoCloseOutline
+              onClick={() => setMenu(false)}
+              className="mt-0 mb-8 text-3xl cursor-pointer"
+            />
+
+            {navlinks.map((d, i) => (
+              <div key={i} className="relative">
+                {d.label === "Categories" ? (
+                  <Link
+                    to={d.link}
+                    className="font-bold"
+                    onClick={() => setCategoriesOpen(!isCategoriesOpen)}
+                  >
+                    {d.label}
+                  </Link>
+                ) : (
+                  <Link to={d.link} className="font-bold">
+                    {d.label}
+                  </Link>
+                )}
+                {d.dropdownOptions &&
+                  d.label === "Categories" &&
+                  isCategoriesOpen && (
+                    <div className="absolute top-full left-0 mt-2 bg-white rounded-md shadow-lg z-10">
+                      {d.dropdownOptions.map((option, index) => (
+                        <Link
+                          key={index}
+                          to={`#${option.toLowerCase()}`}
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        >
+                          {option}
+                        </Link>
+                      ))}
+                    </div>
+                  )}
+              </div>
+            ))}
+
+            {/* Search bar in the sidebar */}
+              <input
+                type="text"
+                placeholder="Search..."
+                className="px-1 py-1 border border-gray-300 rounded-lg focus:outline-none"
+                style={{ width: "90%" }}
+              />
+          </section>
+        </div>
+
+        {/* Right Navbar Section */}
+        <section className="flex items-center gap-4">
+          {/* Search bar */}
+          <input
+            type="text"
+            placeholder="Search..."
+            className="hidden lg:block px-2 py-1 border border-gray-300 rounded-lg focus:outline-none"
+          />
+
+          {/* Cart icon */}
+          <AiOutlineShoppingCart className="text-3xl" />
+
+          {/* Profile icon */}
+          <IoPersonCircle className="text-4xl" />
+        </section>
+      </nav>
+    </main>
+  );
+}
+
+
+
+
