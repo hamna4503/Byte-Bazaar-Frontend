808c86ddcf5acb56c01b514f7d389d8b0ba2dee4
MahamTKhan <mahamtkhan2@gmail.com>
2024-03-12 02:16:36 +0500

    added Footer

---
+++ src/UserPanel/components/Footer/FooterLinks.js
@@ -0,0 +1,24 @@
+//all footer menu items are in this file
+export const WEBSTORE = [
+    { name: "Home", link: "#" },
+    { name: "About Us", link: "#" },
+    { name: "Shop Online", link: "#" },
+  ];
+  export const PRICING = [
+    { name: "Overview", link: "#" },
+    { name: "Premium Plans", link: "#" },
+    { name: "Promotional Offers", link: "#" },
+  ];
+  export const RESOURCES = [
+    { name: "Privacy Policy", link: "#" },
+    { name: "Documentation", link: "#" },
+    { name: "Copyright", link: "#" },
+
+  ];
+  export const SUPPORT = [
+    { name: "Documentation", link: "#" },
+    { name: "Tutorials & guides", link: "#" },
+    { name: "Webinars", link: "#" },
+    { name: "Open-source", link: "#" },
+  ];
+  
