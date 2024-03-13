8804e4360eee5de06ba2d3ed7c6312b241725061
hamna4503 <hamnaaamir593@gmail.com>
2024-03-14 01:38:48 +0500

    Updated breakpoints. Added icon
    Signed-off-by: hamna4503 <hamnaaamir593@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "400px",
+      //This one has is a customized breakpoint entered by Hamna.
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         Purple: "#280130",
@@ -10,5 +29,3 @@ export default {
   },
   plugins: [],
 };
-
-
