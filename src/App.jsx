2c4ac51832db21901c565af2142cdeb3217a63c0
MahamTKhan <mahamtkhan2@gmail.com>
2024-03-23 17:55:53 +0500

    'Shop frontend'

--- src/App.jsx
+++ src/App.jsx
@@ -21,27 +21,16 @@ export default function App() {
           <Route path="/" element={<UserNavbar />} />
           <Route path="/bytebazaar/signup" element={<SignUp />} />
           <Route path="/bytebazaar/login" element={<Login />} />
-          <Route
-            path="/bytebazaar/forgot-password"
-            element={<ForgotPassword />}
-          />
-          <Route
-            path="/bytebazaar/reset-password/"
-            element={<ResetPassword />}
-          />
-          <Route
-            path="/bytebazaar/otp-verification/"
-            element={<OTPVerification />}
-          />
-
+          <Route path="/bytebazaar/forget-password" element={<ForgetPassword />} />
+          <Route path="/bytebazaar/reset-password" element={<ResetPassword />} />
           <Route path="/bytebazaar/foot" element={<Footer />} /> 
 
 
     {/*<BrowserRouter>
     this would work like this but it has to be checked by maham
       <Routes>
-      <Route path="/"  component={<ShopMain/>} />
-      <Route path="/product/:id" component={<SingleProductPage/>} />
+      <Route path="/"  component={ShopMain} />
+      <Route path="/product/:id" component={SingleProductPage} />
       </Routes>
 
       </BrowserRouter>*/}
