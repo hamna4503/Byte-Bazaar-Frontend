9ab0a7c97f6f1d36388d4d9524c875f01ab29fc4
hamna4503 <hamnaaamir593@gmail.com>
2024-03-25 19:55:31 +0500

    Responsive Cart Page
    Signed-off-by: hamna4503 <hamnaaamir593@gmail.com>

---
+++ src/UserPanel/components/Cart/AddToCart/AddToCart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AddToCart() {
+  return (
+    <div>
+      <button className="p-2 bg-Purple text-white w-full rounded-sm">
+        Add to Cart
+      </button>
+    </div>
+  );
+}
+
+export default AddToCart;
