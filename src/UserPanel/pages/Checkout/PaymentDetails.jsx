958dd2a06488ae16a5d81324d93fbc6bb501a636
ummeabiha <rizvi4400977@cloud.neduet.edu.pk>
2024-04-07 21:38:40 +0500

    updated checkout form

--- src/UserPanel/pages/Checkout/PaymentDetails.jsx
+++ src/UserPanel/pages/Checkout/PaymentDetails.jsx
@@ -1,94 +1,61 @@
 export default function PaymentDetails(){
     return (
       <form className="w-full mx-auto px-10">
-        <h2 className="text-Purple text-2xl font-bold mb-8">PAYMENT DETAILS</h2>
+        <h2 className="text-2xl text-center text-black mb-8">
+          Payment Details
+        </h2>
+
         <div className="grid md:grid-cols-2 md:gap-6">
-          <div className="relative z-0 w-full mb-5 group">
+          <div className="relative z-0 w-full group">
             <input
-              type="text"
+              className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-black border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
               name="name"
-              id="name"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required=""
-              autoComplete="off"
-            />
-            <label
-              htmlFor="name"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Cardholder's Name
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="number"
-              name="cardNumber"
-              id="cardNumber"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required=""
-              autoComplete="off"
-            />
-            <label
-              htmlFor="cardNumber"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Card Number
-            </label>
-          </div>
-        </div>
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="text"
-            name="cardNumber"
-            id="cardNumber"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required=""
-            autoComplete="off"
-          />
-          <label
-            htmlFor="cardNumber"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Card Number
-          </label>
-        </div>
-        <div className="grid md:grid-cols-2 md:gap-6">
-          <div className="relative z-0 w-full mb-5 group">
-            <input
               type="text"
+              placeholder="Cardholder's Name"
+              autoComplete="off"
+              required
+              //   onChange={handleChange}
+              // value={orderData.address}
+            />
+          </div>
+          <div className="relative z-0 w-full group">
+            <input
+              className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-black border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              name="email"
+              type="email"
+              placeholder="Card Number"
+              autoComplete="off"
+              required
+              //   onChange={handleChange}
+              // value={orderData.address}
+            />
+          </div>
+        </div>
+
+        <div className="grid md:grid-cols-2 md:gap-6 mt-8 mb-2">
+          <div className="relative z-0 w-full group">
+            <input
+              className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-black border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
               name="expiry"
-              id="expiry"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required=""
-              autoComplete="off"
-            />
-            <label
-              htmlFor="expiry"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Expiry Date
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <input
               type="text"
-              name="cvv"
-              id="cvv"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required=""
+              placeholder="Card's Expiry Date"
               autoComplete="off"
+              required
+              onFocus={(e) => (e.target.type = "date")} // Change type to date when focused
+              onBlur={(e) => (e.target.type = "text")} // Change type back to text when blurred
             />
-            <label
-              htmlFor="cvv"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              CVV/ CVC
-            </label>
+          </div>
+          <div className="relative z-0 w-full group">
+            <input
+              className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-black border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              name="cvv"
+              type="number"
+              placeholder="Card Verification Value"
+              autoComplete="off"
+              required
+              //   onChange={handleChange}
+              // value={orderData.address}
+            />
           </div>
         </div>
       </form>
