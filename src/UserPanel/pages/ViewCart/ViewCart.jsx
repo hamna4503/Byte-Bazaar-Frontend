31a90b3b31efa6a2b87d5bb78dac3a6aeaf5030a
ummeabiha <rizvi4400977@cloud.neduet.edu.pk>
2024-06-08 19:28:22 +0500

    minor changes

--- src/UserPanel/pages/ViewCart/ViewCart.jsx
+++ src/UserPanel/pages/ViewCart/ViewCart.jsx
@@ -15,28 +15,28 @@ function ViewCart() {
     window.location.href = "/bytebazaar/checkout";
   };
   return Cart.length != 0 ? (
-    <div className="w-screen flex flex-col justify-center items-center my-6">
+    <div className="flex flex-col items-center justify-center w-screen my-6">
       {/* Headers for the Cart */}
       <table className="w-11/12 sm:w-4/5">
         <tr className="text-white bg-Purple">
-          <th className="text-left py-2 w-3/5 pl-5">Product</th>
-          <th className="text-center w-1/5">Quantity</th>
-          <th className="text-right w-1/5 px-2">Subtotal</th>
+          <th className="w-3/5 py-2 pl-5 text-left">Product</th>
+          <th className="w-1/5 text-center">Quantity</th>
+          <th className="w-1/5 px-2 text-right">Subtotal</th>
         </tr>
         {/* Cart Items Display  */}
         {Cart && Cart.length > 0 ? <CartItems /> : <>No Items added</>}
       </table>
 
       {/* Cart Total Display */}
-      <div className="flex flex-col w-11/12 sm:w-4/5 items-end">
-        <div className="w-full sm:w-2/3 md:w-2/5 lg:w-1/3 border-t-2 border-t-Purple text-base font-medium py-2">
+      <div className="flex flex-col items-end w-11/12 sm:w-4/5">
+        <div className="w-full py-2 text-base font-medium border-t-2 sm:w-2/3 md:w-2/5 lg:w-1/3 border-t-Purple">
           <CartTotal title="Subtotal" price={Total} />
           <CartTotal title="Tax(2%)" price={Tax} />
           <CartTotal title="Total" price={OrderTotal} />
         </div>
 
         <button
-          className="bg-Purple text-white px-3 py-1 sm:px-10 rounded-xl sm:mt-2 sm:font-semibold"
+          className="px-3 py-1 text-white bg-Purple sm:px-10 rounded-xl sm:mt-2 sm:font-semibold"
           onClick={checkout}
         >
           Proceed to Checkout
@@ -44,7 +44,7 @@ function ViewCart() {
       </div>
     </div>
   ) : (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex items-center justify-center h-screen">
       You have no items in your cart
     </div>
   );
